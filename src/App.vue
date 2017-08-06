c4f18950ee47c3310653a833a68981a8d7819c1d
Thibaut Mottet <thibaut.mottet@gmail.com>
2017-08-06 18:29:29 +0200

    Add Sparkline

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,32 @@
         <div id="page-wrapper" class="gray-bg">
             <div class="wrapper wrapper-content animated fadeInRight">
                 <div class="row">
-                    <div class="col-sm-8">
-                        Col
+                    <div class="col-sm-4">
+                        <h1 class="m-b-xs">
+                            26,900
+                        </h1>
+                        <small>
+                            Sales in current month
+                        </small>
+                        <spark></spark>
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <small class="stats-label">Pages / Visit</small>
+                                <h4>236 321.80</h4>
+                            </div>
+
+                            <div class="col-xs-4">
+                                <small class="stats-label">% New Visits</small>
+                                <h4>46.11%</h4>
+                            </div>
+                            <div class="col-xs-4">
+                                <small class="stats-label">Last week</small>
+                                <h4>432.021</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        MM
                     </div>
                     <div class="col-sm-4">
                         MM
@@ -16,9 +40,12 @@
 </template>
 
 <script>
+  import Spark from './components/Spark.vue'
 
   export default {
     name: 'app',
-    components: {}
+    components: {
+      Spark
+    },
   }
 </script>
---
+++ src/components/Spark.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="sparkline"></div>
+</template>
+
+<script>
+  import sparkline from 'jquery-sparkline'
+  import $ from 'jquery'
+
+  export default {
+    name: 'spark',
+    mounted () {
+      $('#sparkline').sparkline([34, 43, 43, 35, 44, 32, 44, 52], {
+        type: 'line',
+        width: '100%',
+        height: '50',
+        lineColor: '#1ab394',
+        fillColor: 'transparent'
+      });
+    }
+  }
+</script>
